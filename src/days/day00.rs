a29c27e2b229509a0dbb46ae448f655354a69a1f
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2022-12-01 08:21:43 +0000

    added template day 0

---
+++ src/days/day00.rs
@@ -0,0 +1,23 @@
+use crate::{DayResult, IntoDayResult};
+
+pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+    use crate::{Answers, DayResult};
+
+    #[test]
+    fn test_answers() {
+        let result = run(include_str!("../../input/real/01.txt"));
+        assert!(matches!(
+            result,
+            Ok(DayResult {
+                part1: None,
+                part2: None,
+            })
+        ));
+    }
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -4,24 +4,11 @@ use std::ops::{Add, Mul, Sub};
 pub mod day01;
 
 #[inline(always)]
-pub fn byte_slice_to_int<T: Num + From<u8> + Add + Sub + Mul + Clone>(slice: &[u8]) -> T {
+pub fn byte_slice_to_int<T: Num + From<u8> + Add + Sub + Mul>(slice: &[u8]) -> T {
     let mut res = T::zero();
 
-    let ten = {
-        T::one()
-            + T::one()
-            + T::one()
-            + T::one()
-            + T::one()
-            + T::one()
-            + T::one()
-            + T::one()
-            + T::one()
-            + T::one()
-    };
-
     for &b in slice {
-        res = res * ten.clone();
+        res = res * 10.into();
         res = res + b.into() - b'0'.into();
     }
 
