cfdbe7e43092d17a290951ac150e47f7d4627cd5
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2022-12-02 22:23:50 +0000

    big speed

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,20 @@
 use crate::{DayResult, IntoDayResult};
 
+macro_rules! sort_arr {
+    ( $val:expr, $arr:expr ) => {
+        if $val > $arr[0] {
+            $arr[2] = $arr[1];
+            $arr[1] = $arr[0];
+            $arr[0] = $val;
+        } else if $val > $arr[1] {
+            $arr[2] = $arr[1];
+            $arr[1] = $val;
+        } else if $val > $arr[2] {
+            $arr[2] = $val;
+        }
+    };
+}
+
 pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
     let mut calorie_sums = [0; 3];
 
@@ -12,16 +27,7 @@ pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
         match byte {
             b'\n' => {
                 if last_newline + 1 == i {
-                    if sum > calorie_sums[0] {
-                        calorie_sums[2] = calorie_sums[1];
-                        calorie_sums[1] = calorie_sums[0];
-                        calorie_sums[0] = sum;
-                    } else if sum > calorie_sums[1] {
-                        calorie_sums[2] = calorie_sums[1];
-                        calorie_sums[1] = sum;
-                    } else if sum > calorie_sums[2] {
-                        calorie_sums[2] = sum;
-                    }
+                    sort_arr!(sum, calorie_sums);
                     sum = 0;
                 } else {
                     sum += curr_int;
@@ -36,10 +42,30 @@ pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
         }
     }
 
+    sort_arr!(sum, calorie_sums);
+
     let part1 = calorie_sums[0];
     let part2 = calorie_sums.iter().sum::<u32>();
 
     (part1, part2).into_result()
+}
+
+pub fn run2(input: &'static str) -> anyhow::Result<DayResult> {
+    let (p1, p2) = input
+        .split("\n\n")
+        .try_fold((0, [0u32; 4]), |(p1, mut part2_arr), group| {
+            group
+                .lines()
+                .map(|line| line.parse::<u32>())
+                .sum::<Result<u32, _>>()
+                .map(|calories| {
+                    part2_arr[0] = calories;
+                    part2_arr.sort();
+                    (std::cmp::max(p1, calories), part2_arr)
+                })
+        })?;
+
+    (p1, p2.into_iter().skip(1).sum::<u32>()).into_result()
 }
 
 #[cfg(test)]
--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,18 +1,27 @@
 use crate::{DayResult, IntoDayResult};
 
+const LOOKUP_SCORE: [[usize; 3]; 3] = [
+    [4, 1, 7], // rock
+    [8, 5, 2], // paper
+    [3, 9, 6], // scissors
+];
+
+const LOOKUP_MOVE: [[usize; 3]; 3] = [
+    [3, 4, 8], // rock
+    [1, 5, 9], // paper
+    [2, 6, 7], // scissors
+];
+
 pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
     let input = input.as_bytes();
 
     (0..input.len())
         .step_by(4)
-        .map(|i| (input[i], input[i + 2]))
-        .fold((0, 0), |(p1, p2), (first, second)| {
-            let opp = (first - b'A') as i8;
-            let me_or_goal = (second - b'X') as i8;
-            let res = (me_or_goal - opp + 1).rem_euclid(3) * 3;
-            let should_achieve = (me_or_goal - 1 + opp).rem_euclid(3) + 1;
-            let p1 = p1 + (me_or_goal + 1) as u32 + res as u32;
-            let p2 = p2 + should_achieve as u32 + (me_or_goal * 3) as u32;
+        .fold((0, 0), |(p1, p2), i| {
+            let opp = (input[i] - b'A') as usize;
+            let me_or_goal = (input[i + 2] - b'X') as usize;
+            let p1 = p1 + LOOKUP_SCORE[me_or_goal][opp];
+            let p2 = p2 + LOOKUP_MOVE[opp][me_or_goal];
             (p1, p2)
         })
         .into_result()
@@ -29,8 +38,8 @@ mod tests {
         assert_eq!(
             result.unwrap(),
             DayResult {
-                part1: Some(Answers::U32(15)),
-                part2: Some(Answers::U32(12)),
+                part1: Some(Answers::Usize(15)),
+                part2: Some(Answers::Usize(12)),
             }
         );
     }
@@ -41,8 +50,8 @@ mod tests {
         assert_eq!(
             result.unwrap(),
             DayResult {
-                part1: Some(Answers::U32(9241)),
-                part2: Some(Answers::U32(14610)),
+                part1: Some(Answers::Usize(9241)),
+                part2: Some(Answers::Usize(14610)),
             }
         );
     }
