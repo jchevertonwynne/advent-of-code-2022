da38c5ca8046c8b15a2a718ccb80985bc7d17ceb
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2022-12-03 15:19:54 +0000

    rewrote as fold and padded lookup array

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,40 +1,35 @@
 use crate::{DayResult, IntoDayResult};
 use bstr::{BStr, ByteSlice};
 
-const LOOKUP: [u64; 58] = [
+const LOOKUP: [u64; 64] = [
     27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
     51, 52, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26,
+    20, 21, 22, 23, 24, 25, 26, 0, 0, 0, 0, 0, 0,
 ];
 
 pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
-    let mut part1 = 0;
-    let mut part2 = 0;
+    let (res, _) = BStr::new(input).lines().enumerate().fold(
+        ((0_u64, 0_u64), u64::MAX),
+        |((mut part1, mut part2), mut group), (i, line)| {
+            let (a, b) = line.split_at(line.len() / 2);
+            let (a, b) = a
+                .iter()
+                .zip(b.iter())
+                .fold((0_u64, 0_u64), |(acc_a, acc_b), (&a, &b)| {
+                    (acc_a | (1 << (a - b'A')), acc_b | 1 << (b - b'A'))
+                });
 
-    let mut grouped = u64::MAX;
+            part1 += LOOKUP[(a & b).trailing_zeros() as usize];
 
-    let input = BStr::new(input);
-
-    for (i, line) in input.lines().enumerate() {
-        let (a, b) = line.split_at(line.len() / 2);
-        let (a, b) = a
-            .iter()
-            .zip(b.iter())
-            .fold((0_u64, 0_u64), |(acc_a, acc_b), (&a, &b)| {
-                (acc_a | (1 << (a - b'A')), acc_b | 1 << (b - b'A'))
-            });
-
-        part1 += LOOKUP[(a & b).trailing_zeros() as usize];
-
-        grouped &= a | b;
-
-        if i % 3 == 2 {
-            part2 += LOOKUP[grouped.trailing_zeros() as usize];
-            grouped = u64::MAX;
-        }
-    }
-
-    (part1, part2).into_result()
+            group &= a | b;
+            if i % 3 == 2 {
+                part2 += LOOKUP[group.trailing_zeros() as usize];
+                group = u64::MAX;
+            }
+            ((part1, part2), group)
+        },
+    );
+    res.into_result()
 }
 
 #[cfg(test)]
