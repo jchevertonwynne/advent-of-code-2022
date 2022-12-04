09be8ec2318d9e9234b4c59cb34853844078afed
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2022-12-04 12:52:12 +0000

    faster rewrite

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,9 +7,9 @@ const DAY_03: &str = include_str!("../input/real/03.txt");
 const DAY_04: &str = include_str!("../input/real/04.txt");
 
 fn bench_days(c: &mut Criterion) {
-    // c.bench_function("day 01", |b| b.iter(|| day01::run(black_box(DAY_01))));
-    // c.bench_function("day 02", |b| b.iter(|| day02::run(black_box(DAY_02))));
-    // c.bench_function("day 03", |b| b.iter(|| day03::run(black_box(DAY_03))));
+    c.bench_function("day 01", |b| b.iter(|| day01::run(black_box(DAY_01))));
+    c.bench_function("day 02", |b| b.iter(|| day02::run(black_box(DAY_02))));
+    c.bench_function("day 03", |b| b.iter(|| day03::run(black_box(DAY_03))));
     c.bench_function("day 04", |b| b.iter(|| day04::run(black_box(DAY_04))));
 }
 
--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,37 +1,32 @@
-use anyhow::Context;
-use bstr::{BStr, ByteSlice};
-use nom::Slice;
-
 use crate::{DayResult, IntoDayResult};
-use crate::days::byte_slice_to_int;
 
 pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
     let mut part1 = 0;
     let mut part2 = 0;
 
-    for line in BStr::new(input).lines() {
-        let h1 = line.find("-").context("failed to find the first hyphen")?;
-        let mid = line.find(",").context("failed to find the comma")?;
+    let mut nums = [0_usize; 4];
+    let mut curr = 0_usize;
 
-        let second_half = line.slice(mid + 1..);
-        let h2 = second_half.find("-").context("failed to find the second hyphen")?;
-
-        let a: usize = byte_slice_to_int(line.slice(0..h1));
-        let b: usize =  byte_slice_to_int( line.slice(h1 + 1..mid));
-        let c: usize =  byte_slice_to_int(second_half.slice(..h2));
-        let d: usize = byte_slice_to_int( second_half.slice((h2 + 1)..));
-
-        let first = a..=b;
-        let second = c..=d;
-
-        if (first.contains(&c) && first.contains(&d))
-            || (second.contains(&a) && second.contains(&b))
-        {
-            part1 += 1;
-        }
-
-        if first.contains(&c) || first.contains(&d) || second.contains(&a) || second.contains(&b) {
-            part2 += 1;
+    for &b in input.as_bytes() {
+        match b {
+            b'\n' => {
+                let [a, b, c, d] = nums;
+                part1 += ((a >= c && b <= d) || (c >= a && d <= b)) as usize;
+                part2 += ((a <= c && b >= c)
+                    || (a <= d && b >= d)
+                    || (c <= a && d >= a)
+                    || (c <= b && d >= b)) as usize;
+                nums = [0; 4];
+                curr = 0;
+            }
+            b'-' | b',' => {
+                curr += 1;
+            }
+            b'0'..=b'9' => {
+                nums[curr] *= 10;
+                nums[curr] += b as usize;
+            }
+            _ => unreachable!(),
         }
     }
 
@@ -41,7 +36,7 @@ pub fn run(input: &'static str) -> anyhow::Result<DayResult> {
 #[cfg(test)]
 mod tests {
     use super::run;
-    use crate::{DayResult};
+    use crate::DayResult;
 
     #[test]
     fn test_example_answers() {
