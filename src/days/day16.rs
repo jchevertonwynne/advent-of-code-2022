f286ffb792b6087214f2ac06298f8fb31663c060
Joseph Cheverton-Wynne <joseph.cheverton-wynne@bjss.com>
2022-12-20 00:30:23 +0000

    day 16 crimes

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -3,12 +3,14 @@ use fxhash::FxBuildHasher;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::alpha1;
+use nom::combinator::map;
 use nom::multi::separated_list1;
 use nom::sequence::{delimited, preceded, tuple};
 use nom::IResult;
 use std::cmp::{max, min};
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::hash::BuildHasher;
+use std::result;
 
 pub fn run(mut input: &'static str, _: bool) -> anyhow::Result<DayResult> {
     let mut valves = HashMap::with_hasher(FxBuildHasher::default());
@@ -41,7 +43,7 @@ pub fn run(mut input: &'static str, _: bool) -> anyhow::Result<DayResult> {
 
     let turned_on = &mut HashSet::with_hasher(FxBuildHasher::default());
 
-    let part_1 = solve(
+    let part_1 = solve_part1(
         30,
         "AA",
         &valves,
@@ -54,32 +56,164 @@ pub fn run(mut input: &'static str, _: bool) -> anyhow::Result<DayResult> {
 
     turned_on.clear();
 
-    let (part_2, mut history) = solve2(
+    let start = Some(Journey {
+        valve: "AA",
+        remaining_turns: 0,
+        future_flow: 0,
+    });
+    let part_2 = solve_part2(
         26,
-        "AA",
-        "AA",
         0,
         0,
-        0,
-        0,
+        start.clone(),
+        start.clone(),
+        false,
         &valves,
         &distances,
         &worth_turning_on,
         turned_on,
-        0,
-        0,
-        vec![]
     );
 
-    history.iter_mut().for_each(|v| v.1 = 27 - v.1);
-    history.sort_unstable_by(|v1, v2| v1.1.cmp(&v2.1));
-    println!("{:?}", history);
 
     (part_1, part_2).into_result()
 }
 
 #[allow(clippy::too_many_arguments)]
-fn solve<'a, H: BuildHasher>(
+fn solve_part2<'a, H: BuildHasher>(
+    turns_remaining: u64,
+    flow_rate: u64,
+    cumulative: u64,
+    a: Option<Journey>,
+    b: Option<Journey>,
+    flipped: bool,
+    valves: &HashMap<&'a str, Valve, H>,
+    distances: &HashMap<(&'a str, &'a str), u64, H>,
+    worth_turning_on: &[&'a str],
+    turned_on: &mut HashSet<&'a str, H>,
+) -> u64 {
+    if turns_remaining == 0 {
+        return cumulative;
+    }
+
+    let mut result = cumulative + turns_remaining * flow_rate;
+
+    if let Some(Journey {
+        valve: a_valve,
+        remaining_turns: a_remaining_turns,
+        future_flow: a_future_flow,
+    }) = a
+    {
+        if a_remaining_turns == 0 {
+            let new_flow_rate = flow_rate + a_future_flow;
+
+            let mut got_one = false;
+            for &new_a_valve in worth_turning_on {
+                if new_a_valve == a_valve {
+                    continue;
+                }
+
+                let dist = *distances.get(&(a_valve, new_a_valve)).unwrap();
+                if dist >= turns_remaining {
+                    continue;
+                }
+
+                if turned_on.insert(new_a_valve) {
+                    got_one = true;
+                    let new_a_valve_info = valves.get(&new_a_valve).unwrap();
+                    let new_a_rem_dist = dist + 1;
+                    let b = b.clone().unwrap();
+                    let b_rem_dist = b.remaining_turns;
+                    let dist_to_move = min(turns_remaining, min(new_a_rem_dist, b_rem_dist));
+
+                    let new_cumulative = cumulative + new_flow_rate * dist_to_move;
+                    let new_a_future_flow = new_a_valve_info.flow_rate;
+                    let new_turns_remaining = turns_remaining - dist_to_move;
+
+                    let new_a = Some(Journey {
+                        valve: new_a_valve,
+                        remaining_turns: new_a_rem_dist - dist_to_move,
+                        future_flow: new_a_future_flow,
+                    });
+                    let new_b = Some(Journey {
+                        valve: b.valve,
+                        remaining_turns: b.remaining_turns - dist_to_move,
+                        future_flow: b.future_flow,
+                    });
+
+                    let new_res = solve_part2(
+                        new_turns_remaining,
+                        new_flow_rate,
+                        new_cumulative,
+                        new_a,
+                        new_b,
+                        false,
+                        valves,
+                        distances,
+                        worth_turning_on,
+                        turned_on,
+                    );
+                    if new_res > result {
+                        result = new_res;
+                    }
+
+                    turned_on.remove(new_a_valve);
+                }
+            }
+
+            if !got_one {
+                if let Some(Journey {
+                    remaining_turns,
+                    future_flow,
+                    ..
+                }) = b
+                {
+                    let turns_to_do = min(turns_remaining, remaining_turns);
+                    let new_result = cumulative
+                        + new_flow_rate * remaining_turns
+                        + (new_flow_rate + future_flow) * (turns_remaining - turns_to_do);
+                    if new_result > result {
+                        result = new_result;
+                    }
+                }
+            }
+        }
+    }
+
+    if let Some(Journey {
+        valve: b_valve,
+        remaining_turns: b_remaining_turns,
+        future_flow: b_future_flow,
+    }) = b
+    {
+        if b_remaining_turns == 0 && !flipped {
+            let new_res = solve_part2(
+                turns_remaining,
+                flow_rate,
+                cumulative,
+                Some(Journey {
+                    valve: b_valve,
+                    remaining_turns: b_remaining_turns,
+                    future_flow: b_future_flow,
+                }),
+                a,
+                true,
+                valves,
+                distances,
+                worth_turning_on,
+                turned_on,
+            );
+
+            if new_res > result {
+                result = new_res;
+            }
+        }
+    }
+
+    result
+}
+
+#[allow(clippy::too_many_arguments)]
+fn solve_part1<'a, H: BuildHasher>(
     turn: u64,
     current_valve: &'a str,
     valves: &HashMap<&'a str, Valve, H>,
@@ -110,7 +244,7 @@ fn solve<'a, H: BuildHasher>(
             let new_cumulative = cumulative + flow_rate * (dist + 1);
             result = max(
                 result,
-                solve(
+                solve_part1(
                     turn - (dist + 1),
                     worth_checking,
                     valves,
@@ -129,125 +263,11 @@ fn solve<'a, H: BuildHasher>(
     result
 }
 
-#[allow(clippy::too_many_arguments)]
-fn solve2<'a, H: BuildHasher>(
-    turn: u64,
-    a_valve: &'a str,
-    b_valve: &'a str,
-    a_remaining: u64,
-    b_remaining: u64,
-    a_future_flow: u64,
-    b_future_flow: u64,
-    valves: &HashMap<&'a str, Valve, H>,
-    distances: &HashMap<(&'a str, &'a str), u64, H>,
-    worth_turning_on: &Vec<&'a str>,
-    turned_on: &mut HashSet<&'a str, H>,
-    flow_rate: u64,
-    cumulative: u64,
-    history: Vec<(&'a str, u64)>
-) -> (u64, Vec<(&'a str, u64)>) {
-    if turn == 0 {
-        return (cumulative, history);
-    }
-
-    let mut result = cumulative + turn * flow_rate;
-    let mut best_history = history.clone();
-
-    if a_remaining == 0 {
-        for &new_a_valve in worth_turning_on {
-            if new_a_valve == a_valve {
-                continue;
-            }
-
-            let dist = *distances.get(&(a_valve, new_a_valve)).unwrap();
-            if dist >= turn {
-                continue;
-            }
-
-            if turned_on.insert(new_a_valve) {
-                let new_valve = valves.get(&new_a_valve).unwrap();
-                let wanted_dist = dist + 1;
-                let actual_dist = min(wanted_dist, b_remaining);
-                let new_flow_rate = flow_rate + a_future_flow;
-                let new_cumulative = cumulative + new_flow_rate * actual_dist;
-                let new_a_future_flow = new_valve.flow_rate;
-                let new_turn = turn - actual_dist;
-                let mut new_history = history.clone();
-                new_history.push((new_a_valve, turn - wanted_dist));
-                let (a, new_history) = solve2(
-                    new_turn,
-                    new_a_valve,
-                    b_valve,
-                    wanted_dist - actual_dist,
-                    b_remaining - actual_dist,
-                    new_a_future_flow,
-                    b_future_flow,
-                    valves,
-                    distances,
-                    worth_turning_on,
-                    turned_on,
-                    new_flow_rate,
-                    new_cumulative,
-                    new_history,
-                );
-                if a > result {
-                    result = a;
-                    best_history = new_history;
-                }
-
-                turned_on.remove(&new_a_valve);
-            }
-        }
-    }
-
-    if b_remaining == 0 {
-        for &new_b_valve in worth_turning_on {
-            if new_b_valve == b_valve {
-                continue;
-            }
-
-            let dist = *distances.get(&(b_valve, new_b_valve)).unwrap();
-            if dist >= turn {
-                continue;
-            }
-
-            if turned_on.insert(new_b_valve) {
-                let new_valve = valves.get(&new_b_valve).unwrap();
-                let wanted_dist = dist + 1;
-                let actual_dist = min(wanted_dist, a_remaining);
-                let new_flow_rate = flow_rate + b_future_flow;
-                let new_cumulative = cumulative + new_flow_rate * actual_dist;
-                let new_b_future_flow = new_valve.flow_rate;
-                let new_turn = turn - actual_dist;
-                let mut new_history = history.clone();
-                new_history.push((new_b_valve, turn));
-                let (a, new_history) = solve2(
-                    new_turn,
-                    a_valve,
-                    new_b_valve,
-                    a_remaining - actual_dist,
-                    wanted_dist - actual_dist,
-                    a_future_flow,
-                    new_b_future_flow,
-                    valves,
-                    distances,
-                    worth_turning_on,
-                    turned_on,
-                    new_flow_rate,
-                    new_cumulative,
-                    new_history
-                );
-                if a > result {
-                    result = a;
-                    best_history = new_history;
-                }
-
-                turned_on.remove(&new_b_valve);
-            }
-        }
-    }
-
-    (result, best_history)
+#[derive(Clone)]
+struct Journey<'a> {
+    valve: &'a str,
+    remaining_turns: u64,
+    future_flow: u64,
 }
 
 fn dist_to(start: &str, end: &str, valves: &HashMap<&str, Valve, impl BuildHasher>) -> u64 {
@@ -301,6 +321,7 @@ fn parse_row(input: &str) -> IResult<&str, (&str, u64, Vec<&str>)> {
 mod tests {
     use super::run;
     use crate::DayResult;
+    use std::assert_eq;
 
     #[test]
     fn test_example_answers() {
